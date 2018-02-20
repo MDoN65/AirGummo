8ec8fcd2498434fd56d6480d15e38f66012712e4
Ryan <no email>
2018-02-20 16:21:50 -0400

    Flight Bean assembled

--- AirGummo/src/java/beans/Flight.java
+++ AirGummo/src/java/beans/Flight.java
@@ -5,6 +5,7 @@
  */
 package beans;
 
+import java.time.LocalDateTime;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 
@@ -20,6 +21,116 @@ public class Flight {
      * Creates a new instance of Flight
      */
     public Flight() {
+
     }
+    private int flightId;
+    private String airlineName;
+    private String departureCode;
+    private String arrivalCode;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+    private Double totalFlyTime;
+    private int flightStatus;
+    private int seatAvailableFirst;
+    private int seatAvailableBus;
+    private int seatAvailableEco;
+    private String reasonCanceled;
+
+    public int getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(int flightId) {
+        this.flightId = flightId;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public String getDepartureCode() {
+        return departureCode;
+    }
+
+    public void setDepartureCode(String departureCode) {
+        this.departureCode = departureCode;
+    }
+
+    public String getArrivalCode() {
+        return arrivalCode;
+    }
+
+    public void setArrivalCode(String arrivalCode) {
+        this.arrivalCode = arrivalCode;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalDateTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public Double getTotalFlyTime() {
+        return totalFlyTime;
+    }
+
+    public void setTotalFlyTime(Double totalFlyTime) {
+        this.totalFlyTime = totalFlyTime;
+    }
+
+    public int getFlightStatus() {
+        return flightStatus;
+    }
+
+    public void setFlightStatus(int flightStatus) {
+        this.flightStatus = flightStatus;
+    }
+    
+    public int getSeatAvailableFirst() {
+        return seatAvailableFirst;
+    }
+
+    public void setSeatAvailableFirst(int seatAvailableFirst) {
+        this.seatAvailableFirst = seatAvailableFirst;
+    }
+
+    public int getSeatAvailableBus() {
+        return seatAvailableBus;
+    }
+
+    public void setSeatAvailableBus(int seatAvailableBus) {
+        this.seatAvailableBus = seatAvailableBus;
+    }
+
+    public int getSeatAvailableEco() {
+        return seatAvailableEco;
+    }
+
+    public void setSeatAvailableEco(int seatAvailableEco) {
+        this.seatAvailableEco = seatAvailableEco;
+    }
+
+    public String getReasonCanceled() {
+        return reasonCanceled;
+    }
+
+    public void setReasonCanceled(String reasonCanceled) {
+        this.reasonCanceled = reasonCanceled;
+    }
+    
     
 }
